83787aac676a3da6ae8d56bb1fd44fc9e1e7ec1b
Luke Lai <uklelai@gmail.com>
2018-11-20 23:31:42 -0800

    setting up heroku

---
+++ build/precache-manifest.4074ca36b9bed9794943c72de0fca536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff31e69799ad42a00980",
+    "url": "/static/js/main.ff31e697.chunk.js"
+  },
+  {
+    "revision": "fe08e59c9a1d0986f89f",
+    "url": "/static/js/1.fe08e59c.chunk.js"
+  },
+  {
+    "revision": "ff31e69799ad42a00980",
+    "url": "/static/css/main.27f38570.chunk.css"
+  },
+  {
+    "revision": "fe08e59c9a1d0986f89f",
+    "url": "/static/css/1.7320bdf9.chunk.css"
+  },
+  {
+    "revision": "a46bd1bfe4d0e548c88f097e20a257bd",
+    "url": "/index.html"
+  }
+];
